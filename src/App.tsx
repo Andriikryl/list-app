a12de2239b5dc30f91d3830a33ab5c5c18826ee5
Andriikryl <andreyk557@gmail.com>
2023-06-03 19:48:48 +0300

    git-init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+const DEFAULT_TODO_LIST = [
+  { id: 1, name: "task 1", description: "description 1", checked: false },
+  { id: 2, name: "task 2", description: "description 2", checked: false },
+  {
+    id: 3,
+    name: "task 3",
+    description:
+      "so long task description 3 so long task description so long task description so long task description so long task description",
+    checked: true,
+  },
+];
+
+function App() {
+  const [todos, setTodos] = React.useState(DEFAULT_TODO_LIST);
+
+  return (
+    <div className="app_container">
+      <div className="container">todo</div>
+    </div>
+  );
+}
+
+export default App;
